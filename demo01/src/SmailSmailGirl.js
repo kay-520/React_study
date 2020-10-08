6083996e0486a79d7fda04db58454cff832a067f
Karma <wangmh1@digitalchina.com>
2020-10-08 20:03:24 +0800

    day01学习

---
+++ demo01/src/SmailSmailGirl.js
@@ -0,0 +1,64 @@
+import React, {Component, Fragment} from "react";
+import './css/detal.css'
+
+class SmailSmailGirl extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputVal: '',
+            list: ['头部按摩', '拔罐']
+        }
+    }
+
+    render() {
+        return (
+            <Fragment>
+                {/* 正确注释的写法 */}
+                {
+                    //正确注释的写法
+                }
+                <div>
+                    <label htmlFor='karma'>加入服务：</label>
+                    <input id='karma' value={this.state.inputVal} onChange={this.inputChange.bind(this)}/>
+                    <button onClick={this.addList.bind(this)}>增加服务</button>
+                </div>
+                <ul>
+                    {
+                        this.state.list.map((item, index) => {
+                            return <li className='input' onClick={this.deleteItem.bind(this, index)}
+                                       key={index + item}>{item}{index} </li>
+                        })
+                    }
+                </ul>
+            </Fragment>
+        )
+    }
+
+    inputChange(e) {
+        console.info(e.target.value)
+        console.info(this)
+        this.setState({
+            inputVal: e.target.value
+        })
+    }
+
+    //增加列表
+    addList() {
+        this.setState({
+            //ES    语法
+            list: [...this.state.list, this.state.inputVal],
+            inputVal: ''
+        })
+    }
+
+    deleteItem(index) {
+        console.log(index)
+        let list = this.state.list
+        list.splice(index, 1)
+        this.setState({
+            list: list
+        })
+    }
+}
+
+export default SmailSmailGirl
