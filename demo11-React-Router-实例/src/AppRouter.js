31b7fcd7103e9670d7e893b4cd739448dd314671
Karma <wangmh1@digitalchina.com>
2020-11-03 22:01:39 +0800

    动态获取路由配置

--- demo11-React-Router-实例/src/AppRouter.js
+++ demo11-React-Router-实例/src/AppRouter.js
@@ -6,21 +6,30 @@ import Indexs from "./page/Indexs";
 import Work from "./page/Work";
 
 function AppRouter() {
+    let routeConfig = [
+        {path: '/', title: '首页', exact: true, component: Indexs},
+        {path: '/video/', title: '视频教程', exact: false, component: Video},
+        {path: '/work/', title: '技能', exact: false, component: Work}
+    ]
     return (
         <Router>
             <div className="mainDiv">
                 <div className="leftNav">
                     <h3>一级导航</h3>
                     <ul>
-                        <li><Link to="/">首页</Link></li>
-                        <li><Link to="/video/">视频教程</Link></li>
-                        <li><Link to="/work">技能</Link></li>
+                        {routeConfig.map((item, index) => {
+                            return (
+                                <li key={index}><Link to={item.path}>{item.title}</Link></li>
+                            )
+                        })}
                     </ul>
                 </div>
                 <div className="rightMain">
-                    <Route path="/" exact component={Indexs}/>
-                    <Route path="/video/" component={Video}/>
-                    <Route path="/work/" component={Work}/>
+                    {routeConfig.map((item, index) => {
+                        return (
+                            <Route key={index} path={item.path} exact={item.exact} component={item.component}/>
+                        )
+                    })}
                 </div>
             </div>
 
